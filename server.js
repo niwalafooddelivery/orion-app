ed8a6d0d704e7054dded1670343f836c92fe8b40
niwalafooddelivery <cineverse275@gmail.com>
2026-08-02 20:38:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (!API_KEY) {
   process.exit(1);
 }
 
-const GEMINI_MODEL = 'gemini-2.0-flash';
+const GEMINI_MODEL = 'gemini-1.5-flash';
 const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}:generateContent?key=${API_KEY}`;
 
 app.use(express.json());
